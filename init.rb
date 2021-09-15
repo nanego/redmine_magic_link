56b8ae2343fc5d89153994808759dae4b8c42589
Vincent Robert <vincent.robert@nanego.com>
2021-09-15 14:47:43 +0200

    Init new plugin

---
+++ init.rb
@@ -0,0 +1,14 @@
+ActiveSupport::Reloader.to_prepare do
+  # require_dependency 'redmine_magic_link/controllers/...'
+  # require_dependency 'redmine_magic_link/models/...'
+end
+
+Redmine::Plugin.register :redmine_magic_link do
+  name 'Redmine Magic Link'
+  author 'Vincent ROBERT'
+  description 'This is a plugin for Redmine which allows you to send Magic links to users'
+  version '0.0.1'
+  url 'https://github.com/nanego/redmine_magic_link'
+  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.4' if Rails.env.test?
+  requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
+end
