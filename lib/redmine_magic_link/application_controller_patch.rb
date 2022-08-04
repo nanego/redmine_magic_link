79860a1b1f6eb88b89734a73e05ef80c6c7fd2ef
Vincent Robert <vincent.robert@nanego.com>
2022-08-04 11:48:55 +0200

    Avoid error when issue has been deleted

--- lib/redmine_magic_link/application_controller_patch.rb
+++ lib/redmine_magic_link/application_controller_patch.rb
@@ -12,14 +12,18 @@ module PluginMagicLink
       if params[:controller] == 'issues' && params[:action] == 'show' && params[:id].present? && params[:issue_key].present?
         issue_rule = IssueMagicLinkRule.where(magic_link_hash: params[:issue_key]).first
         if issue_rule.present? && issue_rule.issue_id == params[:id].to_i
+
           begin
             issue = Issue.find(params[:id])
           rescue ActiveRecord::RecordNotFound
             render_404
           end
+          render_404 if issue.blank?
+
           issue.create_new_membership_with_magic_link(User.current, params[:issue_key]) unless issue.visible?(User.current)
           issue_rule.magic_link_rule.log_used_link(User.current, issue)
           issue_rule.issue.add_watcher(User.current) if issue_rule.magic_link_rule.set_user_as_watcher?
+
           redirect_to issue_path(id: params[:id])
         end
       end
